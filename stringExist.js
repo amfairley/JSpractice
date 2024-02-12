3a04db4c7ea9255d51860158c7042e5c57c86ab6
amfairley <adamfairley1990@gmail.com>
2024-02-12 06:42:41 +0000

    build: string checker

---
+++ stringExist.js
@@ -0,0 +1,6 @@
+/*
+    Check that the variable is defined AND is not null
+    Check that it is a non-empty string
+    Check if is a string
+*/
+if ((((typeof variableName !== 'undefined' && variableName) && variableName.length > 0)) && typeof variableName.valueOf() === 'string');
