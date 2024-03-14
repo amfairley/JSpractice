c55520b93948908a2e03adb6bdd25cf63328a654
amfairley <adamfairley1990@gmail.com>
2024-03-14 10:35:47 +0000

    build: inital structure of JS code

---
+++ loveMaths/assets/js/script.js
@@ -0,0 +1,17 @@
+function runGame() {}
+
+function checkAnswer() {}
+
+function calculateCorrectAnswer() {}
+
+function incrementScore() {}
+
+function incrementWrongAnswer() {}
+
+function displayAddtionQuestion() {}
+
+function displaySubtractionQuestion() {}
+
+function displayMultiplyQuestion() {}
+
+function displayDivideQuestion() {}
