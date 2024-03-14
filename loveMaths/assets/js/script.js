f129345afa039e0507f4934e6ac8d5b5dc350c5e
amfairley <adamfairley1990@gmail.com>
2024-03-14 10:49:00 +0000

    build: add event listeners for buttons

--- loveMaths/assets/js/script.js
+++ loveMaths/assets/js/script.js
@@ -1,3 +1,27 @@
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Get all the html buttons
+    let buttons = document.getElementsByTagName('button');
+    // Iterate throught the buttons html collection
+    for (let button of buttons) {
+        // Add event listener to buttons
+        button.addEventListener("click", function () {
+            // Check the attribute, if it is the submit button make an alert
+            // Otherwise set the gametype to the value of the data-type
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}!`);
+            }
+
+        })
+
+    }
+})
+
 function runGame() {}
 
 function checkAnswer() {}
