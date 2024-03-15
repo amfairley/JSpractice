f8e7989cf427fe73f9b591c58205c52913369bd1
amfairley <adamfairley1990@gmail.com>
2024-03-15 06:26:59 +0000

    function: add checkAnswer functionality

--- loveMaths/assets/js/script.js
+++ loveMaths/assets/js/script.js
@@ -8,10 +8,10 @@ document.addEventListener("DOMContentLoaded", function () {
     for (let button of buttons) {
         // Add event listener to buttons
         button.addEventListener("click", function () {
-            // Check the attribute, if it is the submit button make an alert
+            // Check the attribute, if it is the submit button, check the answe
             // Otherwise set the gametype to the value of the data-type
             if (this.getAttribute("data-type") === "submit") {
-                alert("You clicked submit!");
+                checkAnswer();
             } else {
                 let gameType = this.getAttribute("data-type");
                 runGame(gameType);
@@ -40,9 +40,43 @@ function runGame(gameType) {
     }
 }
 
-function checkAnswer() {}
+/**
+ * Checks the answer against the first value in the
+ * calculateCorrectAnswer array
+ */
+function checkAnswer() {
+    // Get the guess directly from the DOM
+    let userAnswer = parseInt(document.getElementById('answer-box').value);
 
-function calculateCorrectAnswer() {}
+    // Correct answer
+    let calculatedAnswer = calculateCorrectAnswer();
+
+    // Compare user answer with correct answer array
+    let isCorrect = userAnswer === calculatedAnswer[0];
+    if (isCorrect) {
+        alert("Hey, you got it right :)");
+    } else {
+        alert(`Oh no, the correct answer was actually ${calculatedAnswer[0]}, not ${userAnswer}.`);
+    }
+
+    runGame(calculatedAnswer[1]);
+}
+
+/**
+ * Gets the operand and operator from the dom and returns the correct answer
+ */
+function calculateCorrectAnswer() {
+    let operator = document.getElementById('operator').innerText;
+    let operand1 = parseInt(document.getElementById('operand1').innerText);
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
+
+    if (operator === "+") {
+        return [operand1 + operand2, "addition"];
+    } else {
+        alert `Unrecognised operator ${operator}`;
+        throw `Unrecognised operator ${operator}. Aborting.`;
+    }
+}
 
 function incrementScore() {}
 
